3bc6536e3a213dbf7a4a002407aa77e59a3336b9
Jordan Arscott <jordan.arscott@gmail.com>
2020-01-20 09:59:46 -0500

    Add instructions

--- 166-cat-and-mouse.ts
+++ 166-cat-and-mouse.ts
@@ -1,3 +1,28 @@
+// Daily Challenge #166 - Cat and Mouse
+
+// https://dev.to/thepracticaldev/daily-challenge-166-cat-and-mouse-jk4
+
+// You will be given a string (x) featuring a cat 'C', a dog 'D' and a mouse 'm'. The rest of the string will be made up of '.'.
+// You need to find out if the cat can catch the mouse from its current position. The cat can jump (j) characters.
+// Also, the cat cannot jump over the dog.
+// So:
+// if j = 5:
+// ..C.....m. returns 'Caught!' <-- not more than j characters between
+// .....C............m...... returns 'Escaped!' <-- as there are more than j characters between the two, the cat can't jump far enough
+// if j = 10:
+// ...m.........C...D returns 'Caught!' <--Cat can jump far enough and jump is not over dog
+// ...m....D....C....... returns 'Protected!' <-- Cat can jump far enough, but dog is in the way, protecting the mouse
+
+function main() {
+  const games: { s: string; r: number }[] = [
+    { s: ".....C............m......", r: 5 },
+    { s: "...m.........C...D", r: 10 },
+    { s: "...m....D....C.......", r: 10 }
+  ];
+
+  games.forEach(g => console.log(g, mouseEscaped(g.s, g.r)));
+}
+
 function mouseEscaped(input: string, range: number): string {
   let arr = input.split("");
 
@@ -24,16 +49,6 @@ function isProtected(catPos: number, mousePos: number, dogPos: number): boolean 
   let max = Math.max(catPos, mousePos);
 
   return dogPos > min && dogPos < max;
-}
-
-function main() {
-  const games: { s: string; r: number }[] = [
-    { s: ".....C............m......", r: 5 },
-    { s: "...m.........C...D", r: 10 },
-    { s: "...m....D....C.......", r: 10 }
-  ];
-
-  games.forEach(g => console.log(g, mouseEscaped(g.s, g.r)));
 }
 
 main();
