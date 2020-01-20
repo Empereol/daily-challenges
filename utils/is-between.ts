79890237e0627707c0964d7b445cece2ec8dedcd
Jordan Arscott <jordan.arscott@gmail.com>
2020-01-20 14:01:39 -0500

    🧹

--- utils/is-between.ts
+++ utils/is-between.ts
@@ -1,11 +1,11 @@
 /**
- *  Check to see if the value is between the outside most posts
+ * Check to see if the value is between the outside most posts
  * @param value
  * @param posts
  */
 export function isBetween(value: number, posts: number[]): boolean {
-  let min = Math.min(...posts);
-  let max = Math.max(...posts);
+  const min = Math.min(...posts);
+  const max = Math.max(...posts);
 
   return value > min && value < max;
 }
