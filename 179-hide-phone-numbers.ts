e3199cc1a4d4c5655ec335caf2992d64e761bd6a
Jordan Arscott <jordan.arscott@gmail.com>
2020-02-03 09:21:08 -0500

    Add notes

--- 179-hide-phone-numbers.ts
+++ 179-hide-phone-numbers.ts
@@ -1,3 +1,28 @@
+/*
+Daily Challenge #179 - Hide Phone Numbers
+https://dev.to/thepracticaldev/daily-challenge-179-hide-phone-numbers-5059
+
+Setup
+Implement a function that will hide the last six digits of each phone number. 
+Your function should be able to understand the separators for US numbers. 
+Phone numbers can be separated by spaces, periods, or hyphens.
+
+Examples
+Original: 201-680-0202
+Encrypted: `201-6XX-XXXX
+
+Tests
+encryptNum("328 6587120")
+encryptNum("212-420-0202")
+encryptNum("211-458-7851")
+*/
+
+/*
+My notes:
+I think it makes more sense to return the formatted number as opposed to the format,
+or lack thereof, given.
+*/
+
 import { createPhoneNumber } from './178-create-phone-numbers';
 
 function encryptNum(input: string): string {
