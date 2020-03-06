307e4edc17686e017c03cf1f4d540896c6738e45
Jordan Arscott <jordan.arscott@gmail.com>
2020-03-06 09:30:07 -0500

    Create 22-simple-pig-latin.ts

---
+++ 22-simple-pig-latin.ts
@@ -0,0 +1,26 @@
+/**
+ * Daily Challenge #22 - Simple Pig Latin
+ * https://dev.to/thepracticaldev/daily-challenge-22-simple-pig-latin-3ddl
+ *
+ * Write a function that moves the first letter of each word to the end of the word,
+ * then add "ay". Leave punctuation untouched.
+ *
+ * Examples:
+ * pig_it('Pig latin is cool') # igPay atinlay siay oolcay
+ * pig_it('Hello world !') # elloHay orldway !
+ */
+
+function pigIt(input: string): string {
+  return input.split(/\b/g).reduce((output, word) => {
+    if (word.match(/[^\w]+|_/g)) {
+      return output + word;
+    }
+
+    // Return output with new word pig latin'd
+    // Moves the first letter of the word to the end, then add "ay"
+    return output + word.substring(1) + word.substring(0, 1) + "ay";
+  }, "");
+}
+
+console.log(pigIt("Pig latin is cool ! _ _"));
+console.log(pigIt("Hello world !"));
